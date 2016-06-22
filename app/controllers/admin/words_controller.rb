eca5b45b4ec20bc292d1edb92f7d4ca1d95566c0
huongnguyenmta <huongnt0809@gmail.com>
2016-06-22 13:58:19 +0700

    Admin update word

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,6 +1,11 @@
 class Admin::WordsController < ApplicationController
   before_action :logged_in_user, :verify_admin_user
-  before_action :load_category, only: [:new, :create]
+  before_action :load_category
+  before_action :find_word, only: [:edit, :update, :show]
+
+  def show
+    @ansers = @word.answers
+  end
 
   def new
     @word = Word.new
@@ -17,6 +22,19 @@ class Admin::WordsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @word.update_attributes word_params
+      flash[:success] = t "words.flash_update_success"
+      redirect_to :back
+    else
+      flash[:danger] = t "words.flash_update_fail"
+      render :edit
+    end
+  end
+
   private
   def word_params
     params.require(:word).permit :name, :category_id,
@@ -25,5 +43,9 @@ class Admin::WordsController < ApplicationController
 
   def load_category
     @category = Category.find_by id: params[:category_id]
+  end
+
+  def find_word
+    @word = Word.find_by id: params[:id]
   end
 end
