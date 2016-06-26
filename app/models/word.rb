db7a3b9c7faff5cd52893698ab3859366bc584ca
NghiaNTT <nguyen.thi.trong.nghia@framgia.com>
2016-06-26 15:52:53 +0700

    filter-wordlist

--- app/models/word.rb
+++ app/models/word.rb
@@ -5,4 +5,20 @@ class Word < ActiveRecord::Base
 
   accepts_nested_attributes_for :answers,
     reject_if: lambda {|attribute| attribute[:content].blank?}, allow_destroy: true
+
+  QUERRY_LEARNDED = "id in (select lesson_words.word_id from
+    lesson_words join lessons on lesson_words.lesson_id = lessons.id
+    where lessons.user_id = ?)"
+
+  QUERRY_NOT_LEARNDED = "id not in (select lesson_words.word_id from
+    lesson_words join lessons on lesson_words.lesson_id = lessons.id
+    where lessons.user_id = ?)"
+
+  scope :by_ids, ->ids{where id: ids}
+  scope :not_learned, ->user_id{where QUERRY_NOT_LEARNDED, user_id}
+  scope :learned, ->user_id{where QUERRY_LEARNDED, user_id}
+  scope :alls, ->user_id{}
+  scope :by_category, ->category_id do
+    where category_id: category_id if category_id.present?
+  end
 end
